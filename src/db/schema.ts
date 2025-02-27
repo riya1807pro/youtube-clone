cf8112dbdabc4d130e85be959a7c8182c4c8dce3
riya1807pro <riyakaushik6410@gmail.com>
2025-02-27 11:17:10 +0530

    datbase setuped

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerk_Id: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    //add banner feild
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerk_Id)]
+);
